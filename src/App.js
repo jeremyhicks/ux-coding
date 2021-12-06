2cad35a2e9407255fa5425eeb498f88d4fdb4c89
Jeremy Hicks <mail@jdhicks.com>
2021-12-06 13:25:05 -0800

    Small updates

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import "./styles/styles.scss";
 
 import { Card } from "./components/Card";
-import { Icon } from "./atoms/Icon";
 
 import MenuLeftIcon from "./icons/MenuLeftIcon";
 import TimelineIcon from "./icons/TimelineIcon";
 import AvatarIcon from "./icons/AvatarIcon";
+
+// create header component that accepts Left and Right components
+// style to match the App Bar image
 
 export default function App() {
   return (
     <div className="app">
       <Card>
         <button>Update Status</button>
-        <Icon>
-          <MenuLeftIcon />
-        </Icon>
-        <Icon>
-          <TimelineIcon />
-        </Icon>
+        <MenuLeftIcon />
+        <TimelineIcon />
         <div>Eligibility Review</div>
-        <Icon>
-          <AvatarIcon />
-        </Icon>
+        <AvatarIcon />
         <button>New Loan</button>
       </Card>
     </div>
